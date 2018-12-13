4d97c15662567bdb113e375dc320b584bab2da88
luminousAvocado <jamesckang@gmail.com>
2018-12-13 15:55:30 -0800

    Changed launch activity. Auth activity and layout

---
+++ app/src/main/java/com/example/blippinbloop/fantasy_nfl_stats/FirebaseAuth.java
@@ -0,0 +1,13 @@
+package com.example.blippinbloop.fantasy_nfl_stats;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class FirebaseAuth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
