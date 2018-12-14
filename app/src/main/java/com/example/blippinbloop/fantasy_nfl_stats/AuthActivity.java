b7be65450de1801dbd36a45584f3703a326af72a
luminousAvocado <jamesckang@gmail.com>
2018-12-13 17:12:47 -0800

    email/password fields

--- app/src/main/java/com/example/blippinbloop/fantasy_nfl_stats/AuthActivity.java
+++ app/src/main/java/com/example/blippinbloop/fantasy_nfl_stats/AuthActivity.java
@@ -3,11 +3,14 @@ package com.example.blippinbloop.fantasy_nfl_stats;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class FirebaseAuth extends AppCompatActivity {
+
+public class AuthActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
     }
+
+
 }
