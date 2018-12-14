1f80f1e0c3c6a440d06e2e5801cf51b379d71da1
luminousAvocado <jamesckang@gmail.com>
2018-12-13 18:25:19 -0800

    Signup/Login functionality

--- app/src/main/java/com/example/blippinbloop/fantasy_nfl_stats/AuthActivity.java
+++ app/src/main/java/com/example/blippinbloop/fantasy_nfl_stats/AuthActivity.java
@@ -1,15 +1,87 @@
 package com.example.blippinbloop.fantasy_nfl_stats;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
-public class AuthActivity extends AppCompatActivity {
+public class AuthActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText inputEmail, inputPassword;
+    private Button btnLogin, btnSignup;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputPassword = (EditText) findViewById(R.id.password);
+        btnLogin = (Button) findViewById(R.id.login);
+        btnSignup = (Button) findViewById(R.id.signup);
+
+        findViewById(R.id.login).setOnClickListener(this);
+        findViewById(R.id.signup).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        String userEmail = inputEmail.getText().toString();
+        String userPass = inputPassword.getText().toString();
+
+        int id = view.getId();
+
+        if(id == R.id.login) {
+            mAuth.signInWithEmailAndPassword(userEmail, userPass)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Log.d("Login", "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                startActivity(new Intent(AuthActivity.this, MainActivity.class));
+                            }
+                            else {
+                                Log.w("Login", "signInWithEmail:failure", task.getException());
+                                Toast.makeText(AuthActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+        else if(id == R.id.signup) {
+            mAuth.createUserWithEmailAndPassword(userEmail, userPass)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Log.d("SignUp", "Successful");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                startActivity(new Intent(AuthActivity.this, MainActivity.class));
+                            }
+                            else {
+                                Log.d("Signup", "Not successful");
+                                Toast.makeText(AuthActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                // do stuff
+                            }
+                        }
+                    });
+        }
     }
 
 
