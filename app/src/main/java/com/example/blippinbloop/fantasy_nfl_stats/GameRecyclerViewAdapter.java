acd59cd50b540f2115bc637fd2efb08ef2a059fe
dgonz1206 <dgonz1206@gmail.com>
2018-12-14 00:58:48 -0800

    game list done

--- app/src/main/java/com/example/blippinbloop/fantasy_nfl_stats/GameRecyclerViewAdapter.java
+++ app/src/main/java/com/example/blippinbloop/fantasy_nfl_stats/GameRecyclerViewAdapter.java
@@ -55,11 +55,12 @@ public class GameRecyclerViewAdapter  extends RecyclerView.Adapter<GameRecyclerV
     public class GameViewHolder extends RecyclerView.ViewHolder{
         TextView homeTeam;
         TextView awayTeam;
-        TextView week;
         TextView startTime;
         TextView homeScoreTotal;
         TextView awayScoreTotal;
-        ImageView img;
+        ImageView img1;
+        ImageView img2;
+
 
         public GameViewHolder(View itemView) {
             super(itemView);
@@ -68,9 +69,10 @@ public class GameRecyclerViewAdapter  extends RecyclerView.Adapter<GameRecyclerV
             awayTeam = (TextView) itemView.findViewById(R.id.textView3);
             homeScoreTotal = (TextView) itemView.findViewById(R.id.score1);
             awayScoreTotal = (TextView) itemView.findViewById(R.id.score2);
-//            url = (TextView) itemView.findViewById(R.id.url);
-//            author = (TextView) itemView.findViewById(R.id.author);
-            img = (ImageView) itemView.findViewById(R.id.imageView1);
+
+            img1 = (ImageView) itemView.findViewById(R.id.imageView1);
+            img2 = (ImageView) itemView.findViewById(R.id.imageView2);
+
         }
 
         void bind(final int listIndex) {
@@ -80,14 +82,12 @@ public class GameRecyclerViewAdapter  extends RecyclerView.Adapter<GameRecyclerV
             awayTeam.setText(String.format(mGameItems.get(listIndex).getAwayTeam()));
             homeScoreTotal.setText(String.format(String.valueOf(mGameItems.get(listIndex).getHomeScoreTotal())));
             awayScoreTotal.setText(String.format(String.valueOf(mGameItems.get(listIndex).getAwayScoreTotal())));
-//            url.setText(String.format("URL: %s", mNewsItems.get(listIndex).getImg()));
-//            author.setText(String.format("Author: %s", mNewsItems.get(listIndex).getAuthor()));
-//            img.setText(String.format("Image: %s", mNewsItems.get(listIndex).getUrl()));
-            //img.setImageResource(R.drawable.cardinals);
+            String variableValue1 = mGameItems.get(listIndex).getHomeTeam().toLowerCase();
+            String variableValue2 = mGameItems.get(listIndex).getAwayTeam().toLowerCase();
 
-            Picasso.get()
-                    .load(R.drawable.cardinals)
-                    .into(img);
+            img1.setImageResource(mContext.getResources().getIdentifier(variableValue1, "drawable", mContext.getPackageName()));
+            img2.setImageResource(mContext.getResources().getIdentifier(variableValue2, "drawable", mContext.getPackageName()));
+
 
         }
 
